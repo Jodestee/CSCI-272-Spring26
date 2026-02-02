4756ef1ace9d2b791688ed99a883de2f026b5863
Jodestee Jn Francois <121042842+Jodestee@users.noreply.github.com>
2026-02-01 20:25:25 -0500

    Add files via upload

---
+++ Week-1/Function Practice.cpp
@@ -0,0 +1,20 @@
+int main() {
+    
+double num1, num2;
+
+std::cout << "Enter the first number: ";
+std::cin >> num1;
+
+std::cout << "Enter the second number: ";
+std::cin >> num2;
+
+// Performing operations
+
+double sum = num1 + num2;
+double quotient = num1 / num2;
+
+// Displaying the results
+std::cout << "Sum: " << sum << std::endl;
+std::cout << "Quotient: " << quotient << std::endl;
+return 0;
+}
